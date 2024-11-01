e29be4c2f2affc4097965688b9360f64ac570b0a
hasanoz07 <hasanozz2002@gmail.com>
2024-11-01 12:44:53 +0300

    fix login

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:balonsistem/data/local/local_storage/local_storage_service.dart';
 import 'package:balonsistem/data/models/auth/req/login_req_model.dart';
 import 'package:balonsistem/data/models/auth/resp/login_resp_model.dart';
+import 'package:balonsistem/data/models/auth/resp/refresh_resp_model.dart';
 import 'package:balonsistem/data/network/api/auth_api.dart';
 import 'package:balonsistem/routes/app_pages.dart';
 import 'package:balonsistem/shared/constants/constants_instances.dart';
@@ -21,6 +22,8 @@ class LoginController extends GetxController {
   final AuthApi _authApi = AuthApi();
 
   late final LoginRespModel _loginRespModel;
+
+  late final RefreshRespModel _refreshRespModel;
 
   Future<void> login() async {
     LoginReqModel loginReqModel = LoginReqModel(
@@ -49,8 +52,8 @@ class LoginController extends GetxController {
             _loginRespModel.user.profilePhotoUrl);
         _localStorage.saveData(
             UserDataKeys.compaines.name, _loginRespModel.user.firmalar);
+        await refreshToken();
         LoadingHelpers.hideLoadingDialog();
-        Get.offAllNamed(Routes.navBar);
       } else {
         LoadingHelpers.hideLoadingDialog();
         SnackbarHelpers.showErrorSnackbar(
@@ -60,6 +63,35 @@ class LoginController extends GetxController {
       LoadingHelpers.hideLoadingDialog();
       SnackbarHelpers.showErrorSnackbar(
           "Hata", "Lütfen e-posta ve şifrenizi kontrol ediniz");
+    }
+  }
+
+  Future<void> refreshToken() async {
+    try {
+      final response =
+          await _authApi.refresh(_localStorage.retrieveData("accessToken"));
+      if (response.statusCode == 200) {
+        _refreshRespModel = RefreshRespModel.fromJson(response.data);
+        _localStorage.saveData(
+            UserDataKeys.accessToken.name, _refreshRespModel.accessToken);
+        _localStorage.saveData(
+            UserDataKeys.userId.name, _refreshRespModel.user.id);
+        _localStorage.saveData(
+            UserDataKeys.roleName.name, _refreshRespModel.user.rolename);
+        _localStorage.saveData(
+            UserDataKeys.name.name, _refreshRespModel.user.name);
+        _localStorage.saveData(UserDataKeys.profilePhoto.name,
+            _refreshRespModel.user.profilePhotoUrl);
+        _localStorage.saveData(
+            UserDataKeys.compaines.name, _refreshRespModel.user.firmalar);
+        Get.offAllNamed(Routes.navBar);
+      } else {
+        SnackbarHelpers.showErrorSnackbar(
+            "Hata", "Bir hata oluştu lütfen daha sonra yeniden deneyiniz");
+      }
+    } catch (e) {
+      SnackbarHelpers.showErrorSnackbar(
+          "Hata", "Bir hata oluştu lütfen daha sonra yeniden deneyiniz");
     }
   }
 }
